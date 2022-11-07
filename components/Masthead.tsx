88727223afddc04cf6044bcf82d7683963231412
joyousdevil <frankeli.vincent.camacho@gmail.com>
2022-11-07 19:42:54 +1000

    update masthead

--- components/Masthead.tsx
+++ components/Masthead.tsx
@@ -28,15 +28,15 @@ const Masthead: React.FC = () => {
         playsInline 
         className="z-0 absolute w-full h-full object-cover"
         >
-        <source src="/assets/smoke.mp4" type="video/mp4; codecs=hvc1" />
+        <source src="/assets/winterfeels.mp4" type="video/mp4; codecs=hvc1" />
         <source src="/assets/smoke.webm" type="video/webm; codecs=vp9" />
       </video>
       <div className="absolute top-0 z-10 flex-grow-0 pt-10 md:pt-10 transition-all duration-1000">
         <a href="https://github.com/joyousdevil" className="z-10">
         <Image 
-          src="/assets/github.png" 
-          width={24}
-          height={24}
+          src="/assets/github-filled.svg" 
+          width={50}
+          height={50}
           alt="github repo"
         />
         </a>
@@ -51,10 +51,10 @@ const Masthead: React.FC = () => {
       </div>
       <div className="font-bold z-10 text-stone-100 drop-shadow-[0_5px_3px_rgba(0,0,0,0.4_)] text-center">
         <h1 className="mb-6 font-bold text-5xl xl:text-6xl">
-          TurnKey
+          Joyous Devil Saloon
         </h1>
         <h2 className="mb-5 text-4xl xl:text-5xl tracking-tight">
-          <span>App development, </span> <span>done right.</span>
+          <span>Web development, </span> <span>done right.</span>
         </h2>
       </div>
     </div>
