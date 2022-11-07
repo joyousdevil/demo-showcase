6a68639e66ea0795e01085875d06388346d97f9d
V <frankeli.vincent.camacho@gmail.com>
2022-11-08 07:23:57 +1000

    ts-nocheck

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,7 +1,7 @@
+// @ts-nocheck
 import React from "react"
 import Link from "next/link"
 
-//@ts-ignore
 export const WorkContainer: React.FC = ({children}) => (
     <div className="gride grid-cols-1 lg:grid-cols-2 w-full min-h-screen">
         {children}
@@ -15,7 +15,6 @@ export const WorkBackground: React.FC = () => (
     </div>
 )
 
-//@ts-ignore
 export const WorkLeft: React.FC<{ progress: number }> = ({children, progress}) =>  {
     let translateY = Math.max(0, 50 - progress * 3 * 50)
     if (progress > 0.85) translateY = Math.max(-50, -(progress - 0.85) * 2 * 50)
@@ -30,13 +29,14 @@ export const WorkLeft: React.FC<{ progress: number }> = ({children, progress}) =
     )
 }
 
-// @ts-ignore
 export const WorkRight: React.FC<{ progress: number }> = ({ children, progress }) => {
   let translateY  = Math.max(-50, -(progress - 0.5) * 50) 
   return (
-    <div className="flex flex-1 lg:items-center justify-center h-screen" style={{
+    <div className="flex flex-1 lg:items-center justify-center h-screen" 
+    style={{
         transform: `translateY(${translateY}px)`
-    }}>
+    }}
+    >
         <div className="w-full max-w-md pt-10 lg:pt-0 px-10 md:px-0">
             {children}
         </div>
